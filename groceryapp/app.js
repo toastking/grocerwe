19b0280155e5f99ec8c9779f215b7f915ca7c7f6
Matt Del Signore <mattdelsig@me.com>
2015-02-21 15:04:55 -0500

    changed the title output

--- groceryapp/app.js
+++ groceryapp/app.js
@@ -64,7 +64,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { title: grocery.echo() });
+});
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
